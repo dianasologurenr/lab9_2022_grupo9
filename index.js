c89dac4825ae882e71c3b0bf220505eebfccd65b
Rodrigo Loayza <loayza.rs@pucp.edu.pe>
2022-06-21 23:28:00 -0500

    pregunta 1 - lista de regiones terminada

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-
 $(document).ready(function () {
+    let region = "";
+    $.ajax({
+        type: "GET",
+        url: "https://pokeapi.co/api/v2/region",
+        success: function (response) {
+            $.each(response.results, function (key, value) {
+                region = region + genRegion(key+1, value.name, value.url);
+            });
+            $('#body-paises').html(region);
+        },
+        error: function (ex) {
+            console.log(ex);
+        }
+    });
 
-
+    function genRegion(idx, region, url) {
+        return '<tr>\n' +
+            '       <td>' + idx + '</td>\n' +
+            '       <td>' + region + '</td>\n' +
+            '       <td><a type="button" href="./detalleRegion/detalleRegion.html?region=' + url.split('/').at(-2) + '" class="btn botonDetalle">Detalles</a></td>\n' +
+            '   </tr>'
+    }
 });
